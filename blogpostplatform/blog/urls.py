473cff43f92a715ad3b0469a859f0b19f12cca80
Martin Ivanov <martin.ivanov@mentormate.com>
2024-08-01 19:29:36 +0300

    Add Create post view

--- blogpostplatform/blog/urls.py
+++ blogpostplatform/blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create/', views.create_post, name='create_post'),
     path("", views.post_list, name= "post_list"),
     path('register/', views.register, name='register'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
--- blogpostplatform/blog/views.py
+++ blogpostplatform/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.all()
@@ -20,3 +21,17 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post-create.html', {'form': form})
