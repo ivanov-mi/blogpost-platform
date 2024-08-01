52591ce6d965ec3edd32306af9d2bef6a7555845
Martin Ivanov <martin.ivanov@mentormate.com>
2024-08-01 20:34:56 +0300

    Fix logout functionality

--- blogpostplatform/blogpostplatform/settings.py
+++ blogpostplatform/blogpostplatform/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blogpostplatform.wsgi.application'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = "/"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
