c6b5b5a1555233dd5a91c8ee5bc57a836a17ea47
Martin Ivanov <martin.ivanov@mentormate.com>
2024-08-01 19:03:16 +0300

    Add registration

--- blogpostplatform/blog/views.py
+++ blogpostplatform/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def post_list(request):
     posts = Post.objects.all()
@@ -8,3 +10,13 @@ def post_list(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {'post': post})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
